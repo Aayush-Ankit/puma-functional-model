dafbbb01613dbda660c1282883db32ab0020d47d
Dong Eun Kim <kim2976@msee237lnx1.ecn.purdue.edu>
2019-10-03 18:57:34 -0400

    init

--- mvm.py
+++ mvm.py
@@ -3,47 +3,6 @@ import torch
 import torch.nn as nn
  
 
-def conv2d(input, weight, stride = 1, padding = 0, dilation = 1, groups=1):
-
-    weight_channels_out = weight.shape[0]
-    weight_channels_in = weight.shape[1]
-    weight_row = weight.shape[2]
-    weight_col = weight.shape[3]
-
-    length = weight_channels_in * weight_row * weight_col
-    flatten_weight = weight.reshape((weight_channels_out, length))
-
-    input_channels = input.shape[0]     # weight_channels_in == input_channels
-    input_row = input.shape[1]
-    input_col = input.shape[2]
-    input_pad = torch.zeros((input_channels, input_row + padding*2, input_col + padding*2))
-    if padding == 0:
-        input_pad = input
-    else:
-        input_pad[:, padding:-padding, padding:-padding] = input
-
-
-    output_row = input_row - weight_row + 1 + padding*2
-    output_col = input_col - weight_col + 1 + padding*2
-    output = torch.zeros((weight_channels_out, output_row, output_col))
-
-    for i in range(output_row):
-        for j in range(output_col):
-            flatten_input = input_pad[:, i:i+weight_row, j:j+weight_col].flatten()
-#            input_bits = torch.zeros((flatten_input.length, 16))
-            for k in range(weight_channels_out):
-                output[k,i,j] = sum(flatten_input*flatten_weight[k])
-
-    return output
-
-inputs = np.array([[[1.,0,1],[2,1,0],[1,2,1]],[[2,3,1],[2,0,1],[4,2,1]],[[3,2,1],[0,2,1],[5,3,2]]])
-weights = np.array([[[[2.,1],[1,2]],[[4,2],[0,1]],[[1,0],[3,2]]],[[[2.,1],[1,2]],[[3,2],[1,1]],[[1,2],[3,2]]]])
-
-print(conv2d(inputs,weights, padding=0))
-
-
-
-
 ## 16 bit fixed point
 ##   +---+--------------------+------------------------+
 ##   | 1 |    3    |                12                 |
@@ -281,3 +240,234 @@ print(bit_weight)
 print(1.52*2.252-1.251*1.25)
 a=mvm_simple(input, bit_weight)
 print(bits_to_float16(a))
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Function
+from torch.nn.modules.utils import _pair
+from torch.nn import init
+
+import math
+import numpy as np
+
+class Conv2d_mvm_function(Function):
+
+    # Note that both forward and backward are @staticmethods
+    @staticmethod
+    # bias is an optional argument
+    def forward(ctx, input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
+        #output = F.conv2d(input, weight,  bias, stride, padding, dilation, groups)
+        
+        """"""
+        weight_channels_out = weight.shape[0]
+        weight_channels_in = weight.shape[1]
+        weight_row = weight.shape[2]
+        weight_col = weight.shape[3]
+
+        length = weight_channels_in * weight_row * weight_col
+        flatten_weight = weight.reshape((weight_channels_out, length))
+
+        input_channels = input.shape[0]     # weight_channels_in == input_channels
+        input_row = input.shape[1]
+        input_col = input.shape[2]
+        
+
+        output_row = input_row - weight_row + 1
+        output_col = input_col - weight_col + 1 
+        output = torch.zeros((weight_channels_out, output_row, output_col))
+
+        for i in range(output_row):
+            for j in range(output_col):
+                flatten_input = input[:,:, i:i+weight_row, j:j+weight_col].flatten()
+
+                for k in range(weight_channels_out):
+                    output[k,i,j] = sum(flatten_input*flatten_weight[k])
+
+                    
+        """"""
+        ctx.save_for_backward(input, weight, bias)
+        ctx.stride = stride
+        ctx.padding = padding 
+        ctx.dilation = dilation
+        ctx.groups = groups
+
+        return output
+
+    # This function has only a single output, so it gets only one gradient
+    @staticmethod
+    def backward(ctx, grad_output):
+        # This is a pattern that is very convenient - at the top of backward
+        # unpack saved_tensors and initialize all gradients w.r.t. inputs to
+        # None. Thanks to the fact that additional trailing Nones are
+        # ignored, the return statement is simple even when the function has
+        # optional inputs.
+        input, weight, bias = ctx.saved_tensors
+        
+        stride = ctx.stride
+        padding = ctx.padding 
+        dilation = ctx.dilation
+        groups = ctx.groups
+        grad_input = grad_weight = grad_bias = None
+        
+        # These needs_input_grad checks are optional and there only to
+        # improve efficiency. If you want to make your code simpler, you can
+        # skip them. Returning gradients for inputs that don't require it is
+        # not an error.
+        if ctx.needs_input_grad[0]:
+            grad_input = torch.nn.grad.conv2d_input(input.shape, weight, grad_output, stride, padding, dilation, groups)
+        if ctx.needs_input_grad[1]:
+            grad_weight = torch.nn.grad.conv2d_weight(input, weight.shape, grad_output, stride, padding, dilation, groups) 
+        if bias is not None and ctx.needs_input_grad[2]:
+            grad_bias = grad_output.sum((0,2,3)).squeeze(0)
+            
+
+        return grad_input, grad_weight, grad_bias, None, None, None, None
+
+
+class _ConvNd_mvm(nn.Module):
+
+    __constants__ = ['stride', 'padding', 'dilation', 'groups', 'bias', 'padding_mode']
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride,
+                 padding, dilation, transposed, output_padding,
+                 groups, bias, padding_mode, check_grad=False):
+        super(_ConvNd_mvm, self).__init__()
+        if in_channels % groups != 0:
+            raise ValueError('in_channels must be divisible by groups')
+        if out_channels % groups != 0:
+            raise ValueError('out_channels must be divisible by groups')
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+        self.transposed = transposed
+        self.output_padding = output_padding
+        self.groups = groups
+        self.padding_mode = padding_mode
+
+        if check_grad:
+            tensor_constructor = torch.DoubleTensor # double precision required to check grad
+        else:
+            tensor_constructor = torch.Tensor # In PyTorch torch.Tensor is alias torch.FloatTensor
+
+        if transposed:
+            self.weight = nn.Parameter(tensor_constructor(
+                in_channels, out_channels // groups, *kernel_size))
+        else:
+            self.weight = nn.Parameter(tensor_constructor(
+                out_channels, in_channels // groups, *kernel_size))
+        if bias:
+            self.bias = nn.Parameter(tensor_constructor(out_channels))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        
+        if self.bias is not None:
+            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            init.uniform_(self.bias, -bound, bound)
+
+    def extra_repr(self):
+        s = ('{in_channels}, {out_channels}, kernel_size={kernel_size}'
+             ', stride={stride}')
+        if self.padding != (0,) * len(self.padding):
+            s += ', padding={padding}'
+        if self.dilation != (1,) * len(self.dilation):
+            s += ', dilation={dilation}'
+        if self.output_padding != (0,) * len(self.output_padding):
+            s += ', output_padding={output_padding}'
+        if self.groups != 1:
+            s += ', groups={groups}'
+        if self.bias is None:
+            s += ', bias=False'
+        return s.format(**self.__dict__)
+
+class Conv2d_mvm(_ConvNd_mvm):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1,
+                 bias=True, padding_mode='zeros', check_grad=False):
+        kernel_size = _pair(kernel_size)
+        stride = _pair(stride)
+        padding = _pair(padding)
+        dilation = _pair(dilation)
+        super(Conv2d_mvm, self).__init__(
+            in_channels, out_channels, kernel_size, stride, padding, dilation,
+            False, _pair(0), groups, bias, padding_mode)
+
+    #@weak_script_method
+    def forward(self, input):
+        if self.padding_mode == 'circular':
+            expanded_padding = ((self.padding[1] + 1) // 2, self.padding[1] // 2,
+                                (self.padding[0] + 1) // 2, self.padding[0] // 2)
+
+            return Conv2d_mvm_function.apply(F.pad(input, expanded_padding, mode='circular'),
+                            self.weight, self.bias, self.stride,
+                            _pair(0), self.dilation, self.groups)
+        else:
+            return Conv2d_mvm_function.apply(input, self.weight, self.bias, self.stride,
+                            self.padding, self.dilation, self.groups)
+
+
+
+
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+inputs = torch.tensor([[[[1.,0,1],[2,1,0],[1,2,1]],[[2,3,1],[2,0,1],[4,2,1]],[[3,2,1],[0,2,1],[5,3,2]]]])
+labels = torch.tensor([1])
+weights = np.array([[[[2.,1],[1,2]],[[4,2],[0,1]],[[1,0],[3,2]]],[[[2.,1],[1,2]],[[3,2],[1,1]],[[1,2],[3,2]]]])
+trainloader = [[inputs, labels]]
+#trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform =transforms.Compose([transforms.ToTensor()]))
+#trainloader2 = torch.utils.data.DataLoader(trainset, batch_size=1, shuffle=True, num_workers=4)
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        #self.conv1 = nn.Conv2d(3, 2, 2, bias=False)
+        self.conv1 = Conv2d_mvm(3,2,2, bias=False)
+        self.conv1.weight.data = torch.clone(weights)
+        self.conv1.weight.requires_grad = True
+        #print(weights)
+        self.fc1 = nn.Linear(8, 2, bias=False)
+        self.fc1.weight.data.fill_(0.1)
+
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        print(x)
+        x = x.view(-1, 8)
+        x = self.fc1(x)
+        return x
+
+
+net = Net()
+
+#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#net.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+
+
+for i, data in enumerate(trainloader):
+    # get the inputs; data is a list of [inputs, labels]
+    inputs, labels = data
+    #inputs, labels = inputs.to(device), labels.to(device)
+    outputs = net(inputs)
+
+    print(outputs)
